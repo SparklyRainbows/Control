14e6f7f03dfaee01f7c94bdfdef1c440284e6bf7
jazhang <jazhang@berkeley.edu>
2020-07-10 20:27:19 -0700

    player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMovement : MonoBehaviour {
+    private CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    private float horizontalMove;
+    private bool jump;
+
+    private void Awake() {
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    private void Update() {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
