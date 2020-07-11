165e849c1647d79833fb2f27904268fc1330776a
jazhang <jazhang@berkeley.edu>
2020-07-11 15:27:30 -0700

    UI

---
+++ Assets/Scripts/Movement/TempManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempManager : MonoBehaviour
+{
+    #region CheckItems
+    [SerializeField]
+    private MovementManager Moves;
+    #endregion
+
+    #region GameOver_func
+
+    private void is_GameOver()
+    {
+        if(Moves.getLeftMove() == 0 && Moves.getRightMove() == 0 && Moves.getJumps() == 0)
+        {
+            Debug.Log("Game Over");
+        }
+    }
+    #endregion
+
+    #region Unity_funcs
+    private void Update()
+    {
+        is_GameOver();
+    }
+    #endregion
+}
