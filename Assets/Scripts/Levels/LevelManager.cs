9c29a2aac39e9344d941fb9564ec3dd9dfe73571
jazhang <jazhang@berkeley.edu>
2020-07-10 22:04:40 -0700

    end level flag

---
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public int level;
+
+    private void Start() {
+        LoadLevel(1);
+    }
+
+    public void LoadNextLevel() {
+        level++;
+    }
+
+    public void LoadLevel(int num) {
+        level = num;
+    }
+}
