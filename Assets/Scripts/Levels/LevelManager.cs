ba2a65ebd33442084721c89e50027dee7654c675
Kevin Ponce <kev.ponce4@gmail.com>
2020-07-11 16:25:36 -0700

    Fade made

--- Assets/Scripts/Levels/LevelManager.cs
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     public int level;
-
+    public Animator animator;
     public List<GameObject> levelPrefabs;
     private GameObject currentLevel;
     [SerializeField]
@@ -23,12 +23,13 @@ public class LevelManager : MonoBehaviour
     }
 
     public void LoadNextLevel() {
-        
+        animator.SetTrigger("fade_out");
         LoadLevel(level + 1);
     }
 
     private void LoadLevel(int num) {
-        C.GetComponent<>
+
+        
         if (currentLevel != null) {
             Destroy(currentLevel);
         }
