7ac47e3712f1890f60771ae99b4e60eb22e2c653
Matthew Guo <matthew.guo@gmail.com>
2020-07-11 19:32:17 -0700

    player movement animation added

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour {
     private CharacterController2D controller;
+    private Animator anim;
 
     public float runSpeed = 40f;
 
@@ -18,6 +19,7 @@ public class PlayerMovement : MonoBehaviour {
     private void Awake() {
         controller = GetComponent<CharacterController2D>();
         moves = GetComponent<MovementManager>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update() {
@@ -38,6 +40,8 @@ public class PlayerMovement : MonoBehaviour {
                 horizontalMove = x_input * runSpeed;
                 Debug.Log(horizontalMove);
             }
+            print("left");
+            anim.SetBool("moving", true);
         } else if (x_input > 0 && moves.getRightMove() > 0) {
             if (lateset != 0)
             {
@@ -47,6 +51,8 @@ public class PlayerMovement : MonoBehaviour {
             {
                 horizontalMove = x_input * runSpeed;
             }
+            print("right");
+            anim.SetBool("moving", true);
         } else {
             if (lateset != 0)
             {
@@ -56,6 +62,8 @@ public class PlayerMovement : MonoBehaviour {
             {
                 horizontalMove = 0;
             }
+            print("not moving");
+            anim.SetBool("moving", false);
         }
         if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D)) {
             moves.LowerRightMove();
