455362118aaca8f9a570beff91b400b9317bbd79
Matthew Guo <matthew.guo@gmail.com>
2020-07-11 17:55:03 -0700

    pre merge

--- Assets/Scripts/Levels/LevelManager.cs
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class LevelManager : MonoBehaviour
-{
-    public int level;
-    public Animator animator;
-    public List<GameObject> levelPrefabs;
-    private GameObject currentLevel;
-    [SerializeField]
-    private Canvas C;
-
-    private UI ui;
-
-    private void Start() {
-        ui = GameObject.FindGameObjectWithTag("MoveSet").GetComponent<UI>();
-        print(ui);
-        LoadLevel(level);
-    }
-
-    public void Update() {
-        if (Input.GetKeyDown(KeyCode.R)) {
-            LoadLevel(level);
-        }
-    }
-
-    public void LoadNextLevel() {
-        animator.SetTrigger("fade_out");
-        LoadLevel(level + 1);
-    }
-
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -61,7 +26,6 @@ public class LevelManager : MonoBehaviour
         LoadLevel(level + 1);
     }
 
->>>>>>> 8084e9178b0608af26de781d18a3756a2b183c71
     private void LoadLevel(int num) {
         GameObject.FindGameObjectWithTag("MoveSet").GetComponent<UI>().HideGameOver();
 
