8a8a066656377b52f7c5d6abbca037eeaadd555c
jazhang <jazhang@berkeley.edu>
2020-07-11 17:47:22 -0700

    options

---
+++ Assets/Scripts/Options/ToggleMusic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ToggleMusic : MonoBehaviour, IPointerClickHandler {
+
+    private int index = 0;
+    public Sprite[] images;
+
+    public void OnPointerClick(PointerEventData eventData) {
+        GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>().ToggleMusic();
+
+        index = index == 0 ? 1 : 0;
+        GetComponent<Image>().sprite = images[index];
+    }
+}
